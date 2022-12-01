381385783085c16e9cd51db3c4fc8c59d7aa92e1
zhanglong <zhanglong@163.com>
2022-12-01 12:12:50 +0800

    分支提交

--- src/main/java/Test/one.java
+++ src/main/java/Test/one.java
@@ -4,5 +4,6 @@ public class one {
     public static void main(String[] args) {
         System.out.println("helool");
         System.out.println("hhh1");
+        System.out.println("woaini");
     }
 }
